ce0afe36e96a2d1e8c6b1a14404c35c21b4cac99
rostamrasuli <rostam.homayoon@gmail.com>
2024-08-13 22:56:57 -0400

    utility

--- src/test/java/tek/tdd/utility/SeleniumUtility.java
+++ src/test/java/tek/tdd/utility/SeleniumUtility.java
@@ -1,4 +1,14 @@
 package tek.tdd.utility;
 
-public class SeleniumUtility {
+import org.openqa.selenium.support.ui.WebDriverWait;
+import tek.tdd.base.BaseSetup;
+
+import java.time.Duration;
+
+public class SeleniumUtility extends BaseSetup {
+
+    private WebDriverWait getWait()  {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(WAIT_TIME_IN_SECOND));
+    }
+
 }
