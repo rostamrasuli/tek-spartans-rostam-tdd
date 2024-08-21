338e5d9183fe3510d2f0affaec7da47471d718d8
rostamrasuli <rostam.homayoon@gmail.com>
2024-08-20 20:04:18 -0400

    new changes

---
+++ src/test/java/tek/tdd/tests/CreateAccountTests.java
@@ -0,0 +1,45 @@
+package tek.tdd.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tek.tdd.base.UIBaseClass;
+import tek.tdd.utility.DataGenerator;
+
+public class CreateAccountTests extends UIBaseClass {
+
+    @Test
+    public void createNewAccountTestPositive() {
+        clickOnElement(homePage.signInLink);
+        clickOnElement(signInPage.createNewAccountLink);
+
+        String expectedEmail = DataGenerator.generateRandomEmail("Mohammad");
+
+        signUpPage.fillUpCreateAccountForm("Mohammad",
+                expectedEmail,
+                "Password@123");
+
+        String actualEmail = getElementText(accountProfilePage.accountEmailInfo);
+
+        Assert.assertEquals(actualEmail, expectedEmail,
+                "Profile Page should have same email as Created");
+    }
+
+    /*
+    Story 4.1 (Activity 15 Minute)
+    Navigate to Create Account page and Create new Account
+    With existing email and validate error message "this email is already exist, please use another email address".
+     */
+    @Test
+    public void createNewAccountWithExistingEmail() {
+        clickOnElement(homePage.signInLink);
+        clickOnElement(signInPage.createNewAccountLink);
+
+        signUpPage.fillUpCreateAccountForm("Mohammad", "mohammad2536@gmail.com", "Password!321");
+
+        String actualError = getElementText(signUpPage.signUpError);
+
+        Assert.assertEquals(actualError, "this email is already exist, please use another email address",
+                "Error Message for Existing Email should match");
+
+    }
+}
